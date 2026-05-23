b885e87593b93333e88498c5c52aaf561ffaeac3
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-05-23 23:00:56 +0300

    test OllamaService integration

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from app.services.ollama_service import OllamaService
+
+
 def main():
     print("Hello from ai-domain-checker-api!")
+    ollama = OllamaService()
+    print(ollama.generate("Hi iam ai-domain-checker-api"))
 
 
 if __name__ == "__main__":
