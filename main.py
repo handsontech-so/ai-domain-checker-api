01fa10406b53ed1f21ffc29f6509de28a62eb044
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-05-25 20:29:36 +0300

    Add rate limiting and CORS support to FastAPI application
    - Integrated SlowAPI for rate limiting with a default limit of 10 requests per minute.
    - Added CORS middleware to allow all origins and specific methods (GET, POST).
    - Implemented a custom exception handler for RateLimitExceeded to return appropriate responses.
    - Updated dependencies in pyproject.toml and requirements.txt to include SlowAPI and limits.

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.api.routes import router
 from datetime import datetime
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from fastapi.middleware.cors import CORSMiddleware
+
+limiter = Limiter(key_func=get_remote_address, default_limits=["10/minute"])
 
 app = FastAPI()
+app.state.limiter = limiter
+
+app.add_middleware(SlowAPIMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
+
+
+@app.exception_handler(RateLimitExceeded)
+def rate_limit_exceeded(request: Request, exc: RateLimitExceeded):
+    return _rate_limit_exceeded_handler(request, exc)
 
 
 @app.get("/")
 async def root():
-    return {"health": "ok", "datetime": datetime.now(),"status":"running....."}
+    return {"health": "ok", "datetime": datetime.now(), "status": "running....."}
